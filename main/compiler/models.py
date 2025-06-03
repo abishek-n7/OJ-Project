06d0ec0bef209aa5cee88a4276d3e654b4201e4a
abishek-n7 <abishek0479@gmail.com>
2025-06-03 18:49:40 +0530

    Removed uneccessary folder-main

--- main/compiler/models.py
+++ main/compiler/models.py
@@ -5,8 +5,8 @@ from problems.models import Problem
 # Create your models here.
 
 class CodeSubmission(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE, null=True, blank=True)
     language = models.CharField(max_length=100)
     code = models.TextField()
     input_data = models.TextField(null=True, blank=True)
